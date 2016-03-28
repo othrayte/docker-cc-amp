0cd0177310f922cd4eeac4757db476928e1694c9
Adrian Cowan <othrayte@gmail.com>
2016-03-28 17:39:36 +1100

    Only create a new instance if one doesn't already exist
    Also removed the exec calls and just call the instance manager directly as it creates daemons.
    Also also wait at the end so that the docker container stays alive until the AMP daemon finishes.

--- start_ads.sh
+++ start_ads.sh
@@ -3,12 +3,17 @@ set -e
 
 HOST="0.0.0.0"
 PORT="8080"
+MODULE="ADS"
 
 while [[ $# > 1 ]]
 do
 key="$1"
 
 case $key in
+    -m|--module)
+    MODULE="$2"
+    shift # past argument
+    ;;
     -l|--license-key)
     LICENSE="$2"
     shift # past argument
@@ -36,9 +41,13 @@ esac
 shift # past argument or value
 done
 
-if [ -z ${LICENSE+x} ]; then echo "A license for AMP from cubecoders.com is required and must be specified as --license_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
-if [ -z ${USERNAME+x} ]; then echo "A username must be specified. eg --username amp_user"; exit 1; fi
-if [ -z ${PASSWORD+x} ]; then echo "A password must be specified. eg --password P4$$w0Rd"; exit 1; fi
+if [ ! -d ~/.ampdata/instances/Instance/]; do
+    if [ -z ${LICENSE+x} ]; then echo "A license for AMP from cubecoders.com is required and must be specified as --license_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
+    if [ -z ${USERNAME+x} ]; then echo "A username must be specified. eg --username amp_user"; exit 1; fi
+    if [ -z ${PASSWORD+x} ]; then echo "A password must be specified. eg --password P4$$w0Rd"; exit 1; fi
+    ./ampinstmgr CreateInstance $MODULE Instance $HOST $PORT $LICENSE $PASSWORD +Core.Login.Username $USERNAME
+fi
+./ampinstmgr StartInstance Instance
 
-exec ./ampinstmgr CreateInstance ADS ADSInstance $HOST $PORT $LICENSE $PASSWORD +Core.Login.Username $USERNAME
-exec ./ampinstmgr StartInstance ADSInstance
+# Wait for all AMP processes to finish
+sleep 2; while [ -e /proc/$(pgrep -o AMP) ]; do sleep 5; done
