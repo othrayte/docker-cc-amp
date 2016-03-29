642be87c85b53f64a92347c8a392f5a2e5339c53
Adrian Cowan <othrayte@gmail.com>
2016-03-29 23:39:48 +1100

    Better method of starting modules and configurable instance names

--- start_ads.sh
+++ start_ads.sh
@@ -4,18 +4,23 @@ set -e
 HOST="0.0.0.0"
 PORT="8080"
 MODULE="ADS"
+INSTANCE_NAME="Instance"
 
 while [[ $# > 1 ]]
 do
 key="$1"
 
 case $key in
+    -n|--instance-name)
+    INSTANCE_NAME="$2"
+    shift # past argument
+    ;;
     -m|--module)
     MODULE="$2"
     shift # past argument
     ;;
-    -l|--license-key)
-    LICENSE="$2"
+    -l|--licence-key)
+    LICENCE="$2"
     shift # past argument
     ;;
     -p|--password)
@@ -41,13 +46,13 @@ esac
 shift # past argument or value
 done
 
-if [ ! -d ~/.ampdata/instances/Instance/ ]; then
-    if [ -z ${LICENSE+x} ]; then echo "A license for AMP from cubecoders.com is required and must be specified as --license_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
+if [ -z ${LICENCE+x} ]; then echo "A licence for AMP from cubecoders.com is required and must be specified as --licence_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
+
+if [ ! -d ~/.ampdata/instances/$INSTANCE_NAME/ ]; then
     if [ -z ${USERNAME+x} ]; then echo "A username must be specified. eg --username amp_user"; exit 1; fi
     if [ -z ${PASSWORD+x} ]; then echo "A password must be specified. eg --password P4$$w0Rd"; exit 1; fi
-    ./ampinstmgr CreateInstance $MODULE Instance $HOST $PORT $LICENSE $PASSWORD +Core.Login.Username $USERNAME
+    ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME
+    (cd /ampdata/instances/$INSTANCE_NAME && exec ./AMP_Linux_x86_64)
+else
+    (cd /ampdata/instances/$INSTANCE_NAME && ./AMP_Linux_x86_64 +Core.AMP.LicenceKey $LICENCE && exec ./AMP_Linux_x86_64)
 fi
-./ampinstmgr StartInstance Instance
-
-# Wait for all AMP processes to finish
-sleep 2; while [ -e /proc/$(pgrep -o AMP) ]; do sleep 5; done
