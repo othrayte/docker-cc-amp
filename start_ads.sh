cdc462297d90e68173f258abfd7f6431ed18f7ef
Adrian Cowan <othrayte@gmail.com>
2016-03-30 00:38:30 +1100

    Default username and password & pass through extra options
    Also errors on unexpected options

--- start_ads.sh
+++ start_ads.sh
@@ -4,7 +4,11 @@ set -e
 HOST="0.0.0.0"
 PORT="8080"
 MODULE="ADS"
+USERNAME="admin"
+PASSWORD="password"
 INSTANCE_NAME="Instance"
+
+EXTRAS=()
 
 while [[ $# > 1 ]]
 do
@@ -39,8 +43,13 @@ case $key in
     PORT="$2"
     shift # past argument
     ;;
+    +*)
+    EXTRAS+=("$1" "$2")
+    shift # past argument
+    ;;
     *)
-            # unknown option
+    echo "Unknown option $1"
+    exit 1
     ;;
 esac
 shift # past argument or value
@@ -49,10 +58,11 @@ done
 if [ -z ${LICENCE+x} ]; then echo "A licence for AMP from cubecoders.com is required and must be specified as --licence_key xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; exit 1; fi
 
 if [ ! -d ~/.ampdata/instances/$INSTANCE_NAME/ ]; then
-    if [ -z ${USERNAME+x} ]; then echo "A username must be specified. eg --username amp_user"; exit 1; fi
-    if [ -z ${PASSWORD+x} ]; then echo "A password must be specified. eg --password P4$$w0Rd"; exit 1; fi
-    ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME
+    echo "Creating module instance: ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}"
+    ./ampinstmgr CreateInstance $MODULE $INSTANCE_NAME $HOST $PORT $LICENCE $PASSWORD +Core.Login.Username $USERNAME ${EXTRAS[@]}
+    echo "Starting instance: $INSTANCE_NAME"
     (cd /ampdata/instances/$INSTANCE_NAME && exec ./AMP_Linux_x86_64)
 else
+    echo "Starting instance: $INSTANCE_NAME"
     (cd /ampdata/instances/$INSTANCE_NAME && ./AMP_Linux_x86_64 +Core.AMP.LicenceKey $LICENCE && exec ./AMP_Linux_x86_64)
 fi
